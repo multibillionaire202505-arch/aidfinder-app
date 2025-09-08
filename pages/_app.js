02c37f2ff3c651fc68b037cf00e2a46528a44022
multibillionaire202505-arch <multibillionaire202505@gmail.com>
2025-09-07 19:02:22 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,2 +1,22 @@
-import '../styles/globals.css'
-export default function App({Component,pageProps}){return <Component {...pageProps}/>}
+import { useEffect } from "react";
+import "@/styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  // Register service worker for PWA
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker
+          .register("/sw.js")
+          .then((reg) => {
+            console.log("Service Worker registered:", reg.scope);
+          })
+          .catch((err) => {
+            console.error("Service Worker registration failed:", err);
+          });
+      });
+    }
+  }, []);
+
+  return <Component {...pageProps} />;
+}

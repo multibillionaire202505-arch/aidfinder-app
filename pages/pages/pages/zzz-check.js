0d69220fe588f2306e04479bf1cb09fe4e8ab419
multibillionaire202505-arch <multibillionaire202505@gmail.com>
2025-09-17 19:08:12 -0500

    Create zzz-check.js

---
+++ pages/pages/pages/zzz-check.js
@@ -0,0 +1,8 @@
+export default function ZzzCheck() {
+  return (
+    <main style={{maxWidth:720,margin:"40px auto",padding:16}}>
+      <h1>Routing Check</h1>
+      <p>If you can see this page, the Pages Router is working.</p>
+    </main>
+  );
+}
